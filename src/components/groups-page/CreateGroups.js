60e39d9d3b9f371d0e52c55ffbd77613b6ff5c97
Luong T Quan <luongquan@Luongs-MacBook-Pro.local>
2022-12-08 14:25:53 -0800

    Commit to pulling

--- src/components/groups-page/CreateGroups.js
+++ src/components/groups-page/CreateGroups.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 
 //function to enter group name, group description, public/private choice and picture create.
-function CreateNewGroup(props) {
+export function CreateGroups(props) {
     const [groupName, setGroupName] = useState('');
     const [groupDescription, setGroupDescription] = useState('');
     const [groupPublicity, setGroupPublicity] = useState('Public');
@@ -23,25 +23,15 @@ function CreateNewGroup(props) {
 
     return (
         <div className="create-group">
-            <h2>Create new group</h2>
+            <h1>Create your study group</h1>
             <form onSubmit={handleSubmit}>
                 <label>Group name:</label>
                 <input type="text" required value={groupName} onChange={(e) => setGroupName(e.target.value)} />
                 <label>Group description:</label>
-                <textarea required value={groupDescription} onChange={(e) => setGroupDescription(e.target.value)}></textarea>
-                <select value={groupPublicity} onChange={(e) => setGroupPublicity(e.target.value)}>
-                    <option value="public">Public</option>
-                    <option value="private">Private</option>
-                </select> 
+                <textarea required value={groupDescription} onChange={(e) => setGroupDescription(e.target.value)}></textarea> 
                 {!isPending && <button>Create Group</button>}
                 {isPending && <button>Creating Group</button>}
             </form>
         </div>
-    )
-}
-
-export function CreateGroups(props) {
-    return (
-        < CreateNewGroup/>
     )
 }
